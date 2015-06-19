87e2d072fcfad9c219105b7d57f25553313123a9
tianye.zhang <tianye.zhang@163.com>
2015-06-19 19:02:14 +0800

    3

---
+++ src/main/java/com/wolf/util/Constant.java
@@ -0,0 +1,18 @@
+package com.wolf.util;
+
+
+public interface Constant {
+
+	enum WorkflowDefinition {
+		normalCreditWorkflow
+	}
+
+	public static final String WEB_CONTEXT_PATH = "WEB_CONTEXT_PATH";
+	public static final String WORKFLOW_CONSTANT = "WORKFLOW_CONSTANT";
+	public static final String TASK_URI = "TASK_URI";
+	public static final String DO_SHIFT_WORKFLOW_TASK = "DO_SHIFT_WORKFLOW_TASK";
+	public static final String WORKFLOW_TASK_VARIABLE_MAP = "WORKFLOW_TASK_VARIABLE_MAP";
+	public static final String WORKFLOW_TASK_NAME = "WORKFLOW_TASK_NAME";
+	public static final String WORKFLOW_TASK_URI = "WORKFLOW_TASK_URI";
+
+}
